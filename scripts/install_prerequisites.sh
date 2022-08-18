6e7f5d3fafb8e63277c82493aff0d74425a6d9d7
arnold-keyvalue <arnold@keyvalue.systems>
2022-08-19 00:29:08 +0530

    add: snapcraft yml

---
+++ scripts/install_prerequisites.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if which cloud_sql_prxy &>/dev/null
+then
+{    
+    echo "cloud_sql_proxy is installed"
+}
+else
+{    
+    if [[ "$OSTYPE" != "linux-gnu"* ]]; then
+    {
+        wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
+    }
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        if [ "${arch_name}" = "x86_64" ]; then
+        {
+            curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
+        }
+        else
+        {
+            curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.arm64
+        }
+        fi
+    fi
+    chmod +x cloud_sql_proxy
+    sudo mv cloud_sql_proxy /usr/local/bin/
+}
+fi    
