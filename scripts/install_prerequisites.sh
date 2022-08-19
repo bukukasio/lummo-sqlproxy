b49fb81512506d13c788fb0ff23510c84be6d2c7
arnold-keyvalue <arnold@keyvalue.systems>
2022-08-19 15:14:59 +0530

    fix: post install script

--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -11,6 +11,7 @@ else
         wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
     }
     elif [[ "$OSTYPE" == "darwin"* ]]; then
+    {
         if [ "${arch_name}" = "x86_64" ]; then
         {
             curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
@@ -20,6 +21,7 @@ else
             curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.arm64
         }
         fi
+    }
     fi
     chmod +x cloud_sql_proxy
     sudo mv cloud_sql_proxy /usr/local/bin/
