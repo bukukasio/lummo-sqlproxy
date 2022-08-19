8ffa4599d2aa0e11d2cb508794bd3d99bd5a6419
arnold-keyvalue <arnold@keyvalue.systems>
2022-08-19 15:47:20 +0530

    fix: post install script

--- scripts/install_prerequisites.sh
+++ scripts/install_prerequisites.sh
@@ -7,11 +7,32 @@ then
 else
 {    
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    {
+    {   
+        if which gcloud &>/dev/null; then   
+            echo "gcloud is installed"
+        else
+        {    
+            echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+            sudo apt install apt-transport-https ca-certificates gnupg
+            curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+            sudo apt update && sudo apt install -y google-cloud-sdk
+            gcloud auth application-default login
+        }
+        fi
         wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64 -O cloud_sql_proxy
     }
     elif [[ "$OSTYPE" == "darwin"* ]]; then
     {
+        if which gcloud &>/dev/null; then  
+            echo "gcloud is installed"
+        else
+        {    
+            brew tap caskroom/cask
+            brew cask install google-cloud-sdk
+            source "$(brew --prefix)/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.bash.inc"
+            gcloud auth application-default login
+        }
+        fi
         if [ "${arch_name}" = "x86_64" ]; then
         {
             curl -o cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
@@ -26,4 +47,4 @@ else
     chmod +x cloud_sql_proxy
     sudo mv cloud_sql_proxy /usr/local/bin/
 }
-fi    
+fi
